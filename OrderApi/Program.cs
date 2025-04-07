4926117d75226ec51e454d0ef821caadbc8c3770
Phong Le <phong.ld@3si.vn>
2025-04-07 22:47:08 +0700

    Thiết lập c.RoutePrefix

--- OrderApi/Program.cs
+++ OrderApi/Program.cs
@@ -13,7 +13,11 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Order API V1");
+        c.RoutePrefix = "swagger";
+    });
 }
 
 app.UseHttpsRedirection();
